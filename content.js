9949618d2ecb2514607b0780536c2ffd0d30cf51
Alvin <amx209@gmail.com>
2023-07-31 22:55:12 -0400

    Working SUBWAYS

--- content.js
+++ content.js
@@ -21,19 +21,56 @@ chrome.runtime.onMessage.addListener(
 );
 
 // Shrink the YouTube video player and add a new video player
-window.onload = function() {
+/*window.onload = function() {
     const youtubePlayer = document.querySelector(".html5-video-player");
     if (youtubePlayer) {
-        youtubePlayer.style.width = "50%";
+        youtubePlayer.style.height = "60%";
 
         const newVideoElement = document.createElement('video');
         newVideoElement.id = 'side-video';
-        newVideoElement.style.width = "50%";
+        newVideoElement.style.width = "35%";
         youtubePlayer.parentNode.insertBefore(newVideoElement, youtubePlayer.nextSibling);
 
         const newImageElement = document.createElement('img');
         newImageElement.id = 'side-gif';
-        newImageElement.style.width = "50%";
+        newImageElement.style.width = "100%";
+		newImageElement.style.height = "40%";
+        newImageElement.src = 'https://media1.giphy.com/media/Fr5LA2RCQbnVp74CxH/giphy.gif'; 
         youtubePlayer.parentNode.insertBefore(newImageElement, youtubePlayer.nextSibling);
     }
+}*/
+
+// Function to create and insert the new video and image elements
+function insertMediaElements() {
+    const youtubePlayer = document.querySelector(".html5-video-player");
+    if (youtubePlayer && !document.getElementById('side-video') && !document.getElementById('side-gif')) {
+        youtubePlayer.style.height = "60%";
+
+        const newVideoElement = document.createElement('video');
+        newVideoElement.id = 'side-video';
+        newVideoElement.style.width = "35%";
+        youtubePlayer.parentNode.insertBefore(newVideoElement, youtubePlayer.nextSibling);
+
+        const newImageElement = document.createElement('img');
+        newImageElement.id = 'side-gif';
+        newImageElement.style.width = "100%";
+		newImageElement.style.height = "40%";
+
+        setTimeout(function() {
+            newImageElement.src = 'https://media1.giphy.com/media/Fr5LA2RCQbnVp74CxH/giphy.gif'; 
+            youtubePlayer.parentNode.insertBefore(newImageElement, youtubePlayer.nextSibling);
+        }, 500);
+    }
 }
+
+// Create a MutationObserver instance
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.type === "childList") {
+            insertMediaElements();
+        }
+    });
+});
+
+// Start observing the document with the configured parameters
+observer.observe(document, { childList: true, subtree: true });
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.getElementById('play-button').addEventListener('click', function() {
     const url = document.getElementById('video-url').value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "playVideo", url: url});
+        chrome.tabs.sendMessage(tabs[0].id, {action: "playMedia", url: url});
     });
 });
